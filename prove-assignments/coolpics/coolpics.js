2e65659425535ea459d3122b13211900cc88e977
Isaac614 <moo24006@byui.edu>
2025-05-07 10:37:31 -0600

    began working on javascript for dynamic menu

---
+++ prove-assignments/coolpics/coolpics.js
@@ -0,0 +1,16 @@
+// const menuButton = document.getElementById("menu-button");
+// const nav = document.getElementById("nav");
+// const navElements = document.querySelectorAll("nav a");
+
+// menuButton.addEventListener("click", function() {
+//     if (nav.style.display == "none") {
+//         nav.style.display = "block";
+//         nav.style.gridTemplateColumns = "1fr 1fr 1fr 1fr 1fr"
+//         for (let i = 0; i < navElements.length; i++){
+//             navElements[i].style.display = "block";
+//             navElements[i].style.textAlign = "center";
+//         }
+//     } else {
+//         nav.style.display = "none";
+//     }
+// }); 
