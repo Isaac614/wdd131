259860e5016b78e23c7c0fc6662387548b47cb86
Isaac614 <moo24006@byui.edu>
2025-05-12 10:54:32 -0600

    completed w04 ponder part 1

---
+++ ponder-assignments/w04/javascript-debugging/script.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = calculateArea(radius);
+// console.log(PI);
+// console.log(radius);
+console.log(area);
+
+function calculateArea(radius) {
+    return radius * radius * PI
+}
